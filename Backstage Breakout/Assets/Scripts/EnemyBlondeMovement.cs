ca00598b6ed61f21b692248471ddfa2352dd3d76
mpuglise04 <mjpuglise@gmail.com>
2025-11-18 19:12:23 -0500

    Added Enemy Blonde animation and movement

---
+++ Backstage Breakout/Assets/Scripts/EnemyBlondeMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBlondeMovement : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private Transform leftBoundary;   // Assign empty GameObject
+    [SerializeField] private Transform rightBoundary;  // Assign empty GameObject
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private bool movingRight = true;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        MoveEnemy();
+    }
+
+    private void MoveEnemy()
+    {
+        if (movingRight)
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+            spriteRenderer.flipX = false; // Adjust depending on sprite facing
+            if (transform.position.x >= rightBoundary.position.x)
+                movingRight = false;
+        }
+        else
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+            spriteRenderer.flipX = true; // Flip horizontally when moving left
+            if (transform.position.x <= leftBoundary.position.x)
+                movingRight = true;
+        }
+    }
+}
